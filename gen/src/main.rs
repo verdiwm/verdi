e8021f66367db0aeb4b5f5cc419c917e08676415
Matilde Morrone <contact@morrone.dev>
2024-06-20 23:54:27 +0200

    Add parsing of wayland protocol

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -1,3 +1,102 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+struct Protocol {
+    #[serde(rename = "@name")]
+    name: String,
+    copyright: String,
+    #[serde(default, rename = "interface")]
+    interfaces: Vec<Interface>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+struct Interface {
+    #[serde(rename = "@name")]
+    name: String,
+    #[serde(rename = "@version")]
+    version: usize,
+    description: String,
+    #[serde(default, rename = "request")]
+    requests: Vec<Message>,
+    #[serde(default, rename = "event")]
+    events: Vec<Message>,
+    #[serde(default, rename = "enum")]
+    enums: Vec<Enum>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+struct Message {
+    #[serde(rename = "@name")]
+    name: String,
+    #[serde(rename = "@type")]
+    ty: Option<String>,
+    #[serde(rename = "@since")]
+    since: Option<usize>,
+    #[serde(rename = "@deprecated-since")]
+    deprecated_since: Option<usize>,
+    description: String,
+    #[serde(default, rename = "arg")]
+    args: Vec<Arg>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+struct Arg {
+    #[serde(rename = "@name")]
+    name: String,
+    #[serde(rename = "@type")]
+    ty: String,
+    #[serde(rename = "@interface")]
+    interface: Option<String>,
+    #[serde(rename = "@enum")]
+    r#enum: Option<String>,
+    #[serde(default, rename = "@allow-null")]
+    allow_null: bool,
+    #[serde(rename = "@summary")]
+    summary: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+struct Enum {
+    #[serde(rename = "@name")]
+    name: String,
+    #[serde(default, rename = "@bitfield")]
+    bitfield: bool,
+    #[serde(rename = "@since")]
+    since: Option<usize>,
+    #[serde(rename = "@deprecated-since")]
+    deprecated_since: Option<usize>,
+    description: Option<String>,
+    #[serde(rename = "entry")]
+    entries: Vec<Entry>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+struct Entry {
+    #[serde(rename = "@name")]
+    name: String,
+    #[serde(rename = "@value")]
+    value: String,
+    #[serde(rename = "@summary")]
+    summary: Option<String>,
+    #[serde(rename = "@since")]
+    since: Option<usize>,
+    #[serde(rename = "@deprecated-since")]
+    deprecated_since: Option<usize>,
+}
+
+fn main() -> Result<()> {
+    let xml = fs::read_to_string("wayland/protocol/wayland.xml")?;
+    let protocol: Protocol = quick_xml::de::from_str(&xml)?;
+
+    println!("{protocol:#?}");
+
+    Ok(())
 }
