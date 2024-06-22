480036e2b4e54c4d39a2d76fb36d730ab2654f3f
Federico Maria Morrone <contact@morrone.dev>
2024-06-22 05:26:03 +0200

    Add generated code

---
+++ src/core.rs
@@ -0,0 +1,306 @@
+mod wayland {
+struct wl_display {
+
+                }
+                
+                impl wl_display {
+                    
+                }
+struct wl_registry {
+
+                }
+                
+                impl wl_registry {
+                    
+                }
+struct wl_callback {
+
+                }
+                
+                impl wl_callback {
+                    
+                }
+struct wl_compositor {
+
+                }
+                
+                impl wl_compositor {
+                    
+                }
+struct wl_shm_pool {
+
+                }
+                
+                impl wl_shm_pool {
+                    
+                }
+struct wl_shm {
+
+                }
+                
+                impl wl_shm {
+                    
+                }
+struct wl_buffer {
+
+                }
+                
+                impl wl_buffer {
+                    
+                }
+struct wl_data_offer {
+
+                }
+                
+                impl wl_data_offer {
+                    
+                }
+struct wl_data_source {
+
+                }
+                
+                impl wl_data_source {
+                    
+                }
+struct wl_data_device {
+
+                }
+                
+                impl wl_data_device {
+                    
+                }
+struct wl_data_device_manager {
+
+                }
+                
+                impl wl_data_device_manager {
+                    
+                }
+struct wl_shell {
+
+                }
+                
+                impl wl_shell {
+                    
+                }
+struct wl_shell_surface {
+
+                }
+                
+                impl wl_shell_surface {
+                    
+                }
+struct wl_surface {
+
+                }
+                
+                impl wl_surface {
+                    
+                }
+struct wl_seat {
+
+                }
+                
+                impl wl_seat {
+                    
+                }
+struct wl_pointer {
+
+                }
+                
+                impl wl_pointer {
+                    
+                }
+struct wl_keyboard {
+
+                }
+                
+                impl wl_keyboard {
+                    
+                }
+struct wl_touch {
+
+                }
+                
+                impl wl_touch {
+                    
+                }
+struct wl_output {
+
+                }
+                
+                impl wl_output {
+                    
+                }
+struct wl_region {
+
+                }
+                
+                impl wl_region {
+                    
+                }
+struct wl_subcompositor {
+
+                }
+                
+                impl wl_subcompositor {
+                    
+                }
+struct wl_subsurface {
+
+                }
+                
+                impl wl_subsurface {
+                    
+                }
+}
+mod linux_dmabuf_v1 {
+struct zwp_linux_dmabuf_v1 {
+
+                }
+                
+                impl zwp_linux_dmabuf_v1 {
+                    
+                }
+struct zwp_linux_buffer_params_v1 {
+
+                }
+                
+                impl zwp_linux_buffer_params_v1 {
+                    
+                }
+struct zwp_linux_dmabuf_feedback_v1 {
+
+                }
+                
+                impl zwp_linux_dmabuf_feedback_v1 {
+                    
+                }
+}
+mod presentation_time {
+struct wp_presentation {
+
+                }
+                
+                impl wp_presentation {
+                    
+                }
+struct wp_presentation_feedback {
+
+                }
+                
+                impl wp_presentation_feedback {
+                    
+                }
+}
+mod tablet_v2 {
+struct zwp_tablet_manager_v2 {
+
+                }
+                
+                impl zwp_tablet_manager_v2 {
+                    
+                }
+struct zwp_tablet_seat_v2 {
+
+                }
+                
+                impl zwp_tablet_seat_v2 {
+                    
+                }
+struct zwp_tablet_tool_v2 {
+
+                }
+                
+                impl zwp_tablet_tool_v2 {
+                    
+                }
+struct zwp_tablet_v2 {
+
+                }
+                
+                impl zwp_tablet_v2 {
+                    
+                }
+struct zwp_tablet_pad_ring_v2 {
+
+                }
+                
+                impl zwp_tablet_pad_ring_v2 {
+                    
+                }
+struct zwp_tablet_pad_strip_v2 {
+
+                }
+                
+                impl zwp_tablet_pad_strip_v2 {
+                    
+                }
+struct zwp_tablet_pad_group_v2 {
+
+                }
+                
+                impl zwp_tablet_pad_group_v2 {
+                    
+                }
+struct zwp_tablet_pad_v2 {
+
+                }
+                
+                impl zwp_tablet_pad_v2 {
+                    
+                }
+}
+mod viewporter {
+struct wp_viewporter {
+
+                }
+                
+                impl wp_viewporter {
+                    
+                }
+struct wp_viewport {
+
+                }
+                
+                impl wp_viewport {
+                    
+                }
+}
+mod xdg_shell {
+struct xdg_wm_base {
+
+                }
+                
+                impl xdg_wm_base {
+                    
+                }
+struct xdg_positioner {
+
+                }
+                
+                impl xdg_positioner {
+                    
+                }
+struct xdg_surface {
+
+                }
+                
+                impl xdg_surface {
+                    
+                }
+struct xdg_toplevel {
+
+                }
+                
+                impl xdg_toplevel {
+                    
+                }
+struct xdg_popup {
+
+                }
+                
+                impl xdg_popup {
+                    
+                }
+}
