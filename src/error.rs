f8a48db0acbffb37d35476c6cca01d15b5b26fe7
Matilde Morrone <contact@morrone.dev>
2024-07-06 18:36:27 +0200

    Add io error variant

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,13 @@
+use std::io;
+
 use crate::message::DecodeError;
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("")]
     Malformed(#[from] DecodeError),
+    #[error("")]
+    IoError(#[from] io::Error),
     #[error("")]
     UnknownOpcode,
 }
