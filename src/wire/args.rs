b0eaa462b2c3fc3ea5429d812330a20ca2afb9ee
Matilde Morrone <contact@morrone.dev>
2024-07-07 12:55:03 +0200

    Implement Display for object id

--- src/wire/args.rs
+++ src/wire/args.rs
@@ -35,6 +35,12 @@ impl ObjectId {
     }
 }
 
+impl std::fmt::Display for ObjectId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
 pub struct NewId {
     pub interface: String,
     pub version: u32,
