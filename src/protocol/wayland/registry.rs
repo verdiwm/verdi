06c99cecb260139200b27933ee118434352d7f6a
Matilde Morrone <contact@morrone.dev>
2024-07-08 17:35:13 +0200

    Advertise available formats to clients

--- src/protocol/wayland/registry.rs
+++ src/protocol/wayland/registry.rs
@@ -71,7 +71,13 @@ impl WlRegistry for Registry {
             RegistryGlobals::COMPOSITOR => {
                 client.insert(id.id, Compositor::new(id.id).into_dispatcher())
             }
-            RegistryGlobals::SHM => client.insert(id.id, Shm::new(id.id).into_dispatcher()),
+            RegistryGlobals::SHM => {
+                let shm = Shm::new(id.id);
+
+                shm.advertise_formats(client).await?;
+
+                client.insert(id.id, shm.into_dispatcher())
+            }
             RegistryGlobals::WM_BASE => client.insert(id.id, WmBase::new(id.id).into_dispatcher()),
             _ => return Err(Error::NotFound),
         }
